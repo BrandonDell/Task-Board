24e1789a26a0b967f3f9c5e6c8e1a06c81b28ef7
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-04-13 17:38:40 -0400

    Finalized HTML with Modal and Form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,52 @@
-$( function() {
-    $( "#datepicker" ).datepicker({
-      changeMonth: true,
-      changeYear: true
-    });
-
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
-
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+const addTaskBtn = $('.btn');
+const modal = $('.modal');
+const saveBtn = $('.save-btn');
 
-// Todo: create a function to generate a unique task id
+// Todo: create a function to generate a unique task id....done
 function generateTaskId() {
-    task.id = generateTaskId();
+    return keyGenerator(new Date());
 }
 
-// Todo: create a function to create a task card
-// let userOject = {}
-    function createTaskCard(task) {
-    // need to add a button to delete a task
-    // creates html for the task card
-    let card = document.createElement('div');
-    card.className = 'task-card';
-    card.id = task.id;
+// Todo: create a function to create a task card....need help with this functionz (need to use dayjs)
+// Function needs to: retrieve the string of data from local storage(userTasks), parse the string into an object
+saveBtn.on('click', function createTaskCard(task) {
+    console.log(),
+    const stringCard = localStorage.getItem('userTasks')
+    const newCards = JSON.parse(stringCard);
+    const placement = $('#todo-cards');
 
-    let title = document.createElement('h3');
-    title.textContent = task.name;
+    for (i = 0; i < newCards.length; i++) {
+        let div = $('<div>');
+        let cardTitle = $('<h2>').text(newCards[i].Title);
+        let cardDesc = $('<p>').text(newCards[i].Description);
+        let cardDue = $('<h4>').text(newCards[i].DueDate);
 
-    let description = document.createElement('p');
-    description.textContent = task.description;
+        div.append(cardTitle);
+        div.append(cardDesc);
+        div.append(cardDue);
+        placement.append(div);
+    }
+  
+}),
+// function needs to retrieve string, parse into object, object(formData) into an array with title, desc, due date
+    saveBtn.on('click', function () {
+    let stringTasks = localStorage.getItem('userTasks');
+    let userTasks = JSON.parse(stringTasks) || [];
 
-    //  append title and desctiption to the card....should it be appendChild?
-    card.appendChild(title);
-    card.appendChild(description);
-
-    // append the card to the container
-    let container = document.getElementById('container');
-    container.append(card);
-}
+    const formData = {
+        Title: $('#task-title').val(),
+        Description: $('#task-description').val(),
+        DueDate: $('#task-due-date').val()
+    };
+// push formData object into userTask array back into local storage
+    userTasks.push(formData);
+    localStorage.setItem('userTasks', JSON.stringify(userTasks));
+});
+   
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -71,7 +74,8 @@ function handleDeleteTask(event){
     // rerender the boc using the Render Task Card
     if (!taskCard) return;
     let taskId = taskCard.id;
-// remove task card from the Dom
+    document.querySelector('button').onClick = run;
+    // remove task card from the Dom
     taskCard.remove();
     // or remove the task from the tasks array
     // tasks = task.filter(task => task.id !== taskId);
@@ -86,11 +90,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
-    // clear the modal when "Add Task" is clicked
-    // add localStorage
-    // call the 
-    
-   
-      } );
+    $('#add-task-btn').onClick('click', handleAddTask);
+    renderTaskList();
 });
+
