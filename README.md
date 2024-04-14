eb6bdbbd8d72a0f12cbe6efe97f21b998fc442e9
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-04-14 19:16:49 -0400

    Made adjustments with Tutor session with Chris on JS functioning

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# Task-Board
-Responsibility Tracker and Organizer
+# Task Board
+ 
+## Motivation
+
+The motivation to create this task board application is to prioritize tasks and create organziation with a task list and associated due dates. 
+
+## What does it solve and provide?
+This application will help organzie and display a list of tasks that need to be complete by a specific date. Many tasks and assignments can be overwhelming and having a simple way to track and organize these responsibilties will help reduce stress and amke for a more efficent process. 
+
+
+## Features
+This webisite allows a users the ability to create a to-do list that has a title, due date, and description. Each task will have the ability to move from three categories from To Do, In Progress, and Done. 
+
+ ## Installation 
+ Clone the respository to your Github account and deploy the website once finished and approved code. 
+ 
+ Website Link: https://brandondell.github.io/Task-Board/
+ GitHub: https://github.com/BrandonDell/Task-Board
+ 
+ ## Image
+ ![DellCioppia](./assets/images/taskBoard.png)
+
+ ## Credits
+ -UCF Coding Bootcamp
+ -Tutor Chris assisted debuging 
+
+ ## License
+ N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,145 +2,129 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-const addTaskBtn = $('.btn');
-const modal = $('.modal');
-const saveBtn = $('.save-btn');
+const timeDisplayEl = $('#time-display');
+const taskDisplayEl = $('#task-display');
+const taskFormEl = $('#task-form');
+const titleInputEl = $('#task-title');
+const descriptionInputEl = $('#task-description');
+const dueDateInputEl = $('#task-due-date');
 
 const tStatus = {
-    todo: 'to-do';
-    prog: 'in-progress',
-    done: 'done',
+  todo: 'to-do',
+  prog: 'in-progress',
+  done: 'done',
+};
+function displayTime() {
+    const rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+    timeDisplayEl.text(rightNow);
 }
-
-// Todo: create a function to generate a unique task id....done
 function generateTaskId() {
     return keyGenerator(new Date());
 }
-
-// Todo: create a function to create a task card....need help with this functionz (need to use dayjs)
-// Function needs to: retrieve the string of data from local storage(userTasks), parse the string into an object
-saveBtn.on('click', function createTaskCard(task) {
-    let cardData = {
-        class: '',
-        id: task.id,
-        status: task.status,
-    }
-    let card = generateElement("div", cardData);
-    const now = dayjs(new Date);
-    const dueDate = dayjs(task.duedate);
-    const deltaTime = dueDate.diff(now, 'day');
-    const stringCard = localStorage.getItem('userTasks')
-    const newCards = JSON.parse(stringCard);
-    const placement = $('#todo-cards');
-
-    for (i = 0; i < newCards.length; i++) {
-        let div = $('<div>');
-        let cardTitle = $('<h2>').text(newCards[i].Title);
-        let cardDesc = $('<p>').text(newCards[i].Description);
-        let cardDue = $('<h4>').text(newCards[i].DueDate);
-
-        div.append(cardTitle);
-        div.append(cardDesc);
-        div.append(cardDue);
-        placement.append(div);
-    }
   
-}),
-// need to retrieve string, parse into object, object(formData) into an array with title, desc, due date
-    saveBtn.on('click', function () {
-    let stringTasks = localStorage.getItem('userTasks');
-    let userTasks = JSON.parse(stringTasks) || [];
-
-    const formData = {
-        Title: $('#task-title').val(),
-        Description: $('#task-description').val(),
-        DueDate: $('#task-due-date').val()
-    };
-// push formData object into userTask array back into local storage
-    userTasks.push(formData);
-        localStorage.setItem('userTasks', JSON.stringify(userTasks));
-        return card;
-});
-   
-
-// Todo: create a function to render the task list and make cards draggable/sortable
-function renderTaskList() { 
-    // should get tasks from storage
-    let tasks = getItem('tasks');
-    if (!tasks || tasks.length == 0) {
-        return;
+function generateTaskId() {
+    let task = JSON.parse(localStorage.getItem('task'));
+    if (!task) {
+        task = [];
     }
-    // we need to get the card sections we want to append to card based on status
-    // need to be drop/draggable
-    const todo = $("#todo-cards").empty().sortable();
-    const inProgress = $("#in-progess-cards").empty().sortable();
-    const done = $("#done-cards").empty().sortable();
-
-    tasks.forEah(task => {
-        let card = creatTaskCard(task);
-        card.draggable({
-            containment: '#taskboard',
-            revert: "invalid",
-            snap: true,
-            snapMode: "inner",
-        });
-        
-    // switch handles which card task will attach to
-    switch (task.status) {
-        case tStatus.todo:
-            todo.append(card);
-            break;
-        case tStatus.prog:
-            inProgress.append(card);
-            break;
-        case tStatus.done:
-            done.append(card);
-            break;
-        default:
-            break;
-    }
-// need to make handles drop and draggable 
-    }
-
-
-
-// Todo: create a function to handle adding a new task
-// get their values from user input
-function handleAddTask(event){
-    event.preventDefault();
-    let task = {
-        title: $("#task-title").val(),
-        dueDate: $("#task-due-date").val(),
-        description: $("task-description").val(),
-        id: generateTaskId(),
-        // new task will have its status set to todo by default
-        status: sStatus.todo,
- }
+    return task;
 }
+function saveTaskToStorage(task) {
+    localStorage.setItem('task', JSON.stringify(task));
+  }
+
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+    const taskCard = $('<div>')
+    .addClass('card task-card draggable my-3')
+    .attr('data-task-id', task.id);
+  const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+  const cardBody = $('<div>').addClass('card-body');
+  const cardDescription = $('<p>').addClass('card-text').text(task.type);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+  const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-project-id', task.id);
+  cardDeleteBtn.on('click', handleDeleteTask);
+
+}
+
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {
+    if (task.dueDate && project.status !== 'done') {
+        const now = dayjs();
+        const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+        if (now.isSame(taskDueDate, 'day')) {
+            taskCard.addClass('bg-warning text-white');
+        } else if (now.isAfter(taskDueDate)) {
+            taskCard.addClass('bg-danger text-white');
+            cardDeleteBtn.addClass('border-light');
+        }
+
+    }
+
+    cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+    taskCard.append(cardHeader, cardBody);
+    return taskCard;
+}
+
+function printTaskData() {
+    const task = readTasFromStorage();
+  
+    const todoList = $('#todo-cards');
+    todoList.empty();
+  
+    const inProgressList = $('#in-progress-cards');
+    inProgressList.empty();
+  
+    const doneList = $('#done-cards');
+    doneList.empty();
+
+    for (let taskCard of task) {
+        if (task.status === 'to-do') {
+            todoList.append(createTaskCard(task));
+        } else if (task.status === 'in-progress') {
+            inProgressList.append(createTaskCard(task));
+        } else if (task.status === 'done') {
+            doneList.append(createProjectCard(project));
+        }
+    }
+}
+
+$('.draggable').draggable({
+    opacity: 0.7,
+    zIndex: 100,
+    helper: function (e) {
+      const original = $(e.target).hasClass('ui-draggable')
+        ? $(e.target)
+        : $(e.target).closest('.ui-draggable');
+      return original.clone().css({
+        width: original.outerWidth(),
+      });
+    },
+  });
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-    let taskCard = event.target.closet('.task-card');
-    // delete the box when delete is clicked....onClick
-    // rerender the boc using the Render Task Card
-    if (!taskCard) return;
-    let taskId = taskCard.id;
-    document.querySelector('button').onClick = run;
-    // remove task card from the Dom
-    taskCard.remove();
-    // or remove the task from the tasks array
-    // tasks = task.filter(task => task.id !== taskId);
-
-}
-
-    // Todo: create a function to handle dropping a task into a new status lane
-    // this function will dete the task
+  function handleDeleteProject() {
+    const taskId = $(this).attr('data-task-id');
+    const task = readTaskFromStorage();
+// Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
+    const task = readTaskFromStorage();
+    const taskId = ui.draggable[0].dataset.taskId;
+    const newStatus = event.target.id;
 
+    for (let task of task) {
+        if (task.id === taskId) {
+          task.status = newStatus;
+        }
+      }
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-    $('#add-task-btn').on('click', handleAddTask);
-    renderTaskList();
+    $('#taskDueDate').datepicker({
+        changeMonth: true,
+        changeYear: true,
+    }
 });
-
